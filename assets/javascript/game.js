58539af84ff7d2644d35581a6f68c627836026e2
0jordanmiller@gmail.com <0jordanmiller@gmail.com>
2018-05-03 18:31:19 -0400

    beta

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@
 // Possible breeds to be chosen
 var dogBreeds = ['Golden Retriever', 'German Shepherd', 'Bichon-Frise', 'Poodle', 'Chihuahua', 'Beagle', 'Greyhound', 'Pitbull', 'Husky', 'Shiba Inu', 'Great Dane', 'Rottweiler', 'Boxer']
 
+
 // Choose random dog breed from array
 var dogWord = dogBreeds[Math.floor(Math.random()*dogBreeds.length)];
 
@@ -9,32 +10,96 @@ var dogWord = dogBreeds[Math.floor(Math.random()*dogBreeds.length)];
 var dogWordLength = dogWord.length;
 
 // Create underscores matching the character length of dog breed
-var dogWordUnderscores = '_ '.repeat(dogWordLength);
- 
+// var dogWordUnderscores = '_ '.repeat(dogWordLength);
+var underscoreArray = []
+
+for (i = 0; i < dogWordLength; i++) {
+	underscoreArray.push('_');
+}
+
 // Array containing the letters already guessed by player
 var lettersGuessed = [];
+
 // Indicates how many guesses player has left
 var guessesLeft = 12;
+
 // Indicates player wins
 var wins = 0;
+var indexes = []
+var turn = false;
+var keyPress;
 
 document.onkeyup = function(event) {
-	var keyPress = event.key;
-	lettersGuessed.push(' ' + keyPress);
-	
-
-	if (guessesLeft > 0) {
-		guessesLeft--
-	} else if ( guessesLeft === 0) {
-
+	var body = '<h1>Current Word: ' + underscoreArray.join(' ') + '</h1>' +
+	'<h1>Letters guessed: ' + lettersGuessed + '</h1>' +
+	'<h1>Guesses left: ' + guessesLeft + '</h1>' +
+	'<h1>Wins: ' + wins + '</h1>';
+	var keyPressInit = event.which;
+	console.log('this is', keyPressInit);
+	if (keyPressInit === 32) {
+		turn = true;
+	}
+	if (turn === true) {
+		document.querySelector("#game").innerHTML = body;
+		keyPress = event.key;
+	} else {
+		keyPress = '';
 	}
 
 
-	var body = '<h1>Current Word: ' + dogWordUnderscores + '</h1>' +
-			'<h1>Letters guessed: ' + lettersGuessed + '</h1>' +
-			'<h1>Guesses left: ' + guessesLeft + '</h1>' + 
-			'<h1>Wins: ' + wins + '</h1>';
-	document.querySelector("#game").innerHTML = body;
+	var dogBreeds = ['Golden Retriever', 'German Shepherd', 'Bichon-Frise', 'Poodle', 'Chihuahua', 'Beagle', 'Greyhound', 'Pitbull', 'Husky', 'Shiba Inu', 'Great Dane', 'Rottweiler', 'Boxer']
+	var dogWordLength = dogWord.length;
+	lettersGuessed.push(' ' + keyPress);
+
+	var a = false;
+	for (i = 0; i < dogWordLength; i++) {
+		if (dogWord[i].toLowerCase() === keyPress.toLowerCase()) {
+			underscoreArray[i] = keyPress;
+			a = true;
+		}
+	}
+
+	console.log(underscoreArray);
+
+	// for (k = 0; k < keyPress.length; k++) {
+	// 	if (dogWord[i].toLowerCase() !== keyPress.toLowerCase())
+	// }
+
+
+	
+
+	console.log(dogWord)
+	console.log(underscoreArray.join(''))
+	if (underscoreArray.join('') === dogWord.toLowerCase()) {
+		wins++;
+		guessesLeft = 12;
+		lettersGuessed = [];
+		underscoreArray = [];
+		dogWord = dogBreeds[Math.floor(Math.random()*dogBreeds.length)];
+		dogWordLength = dogWord.length;
+		indexes = [];
+		for (i = 0; i < dogWordLength; i++) {
+			underscoreArray.push('_');
+		}
+	}
+
+	 if (a === false) {
+			guessesLeft--
+		} 
+	if (guessesLeft === 0 || dogWord === 0) {
+		guessesLeft = 12;
+		lettersGuessed = [];
+		underscoreArray = [];
+		dogWord = dogBreeds[Math.floor(Math.random()*dogBreeds.length)];
+		dogWordLength = dogWord.length;
+		indexes = [];
+		for (i = 0; i < dogWordLength; i++) {
+			underscoreArray.push('_');
+		}
+	}
+	
+	
+
 	
 }
 
